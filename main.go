132854c6bc22572768ec56fd77cac608b8fa78ac
Glen Clarkson <29312988+DMXMax@users.noreply.github.com>
2020-03-01 13:54:39 -0800

    I think the example is complete

--- main.go
+++ main.go
@@ -7,26 +7,28 @@ import (
 )
 
 func main() {
-	const msg1 = "Hello World"
-	const salt1 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
-	
-	const msg2 = "7d9ad9f146eHello World"
-	const salt2 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b27"
-	
+	fmt.Println("Experiment #1*************")
+	const msg1 = "My Secret Data"
+	const salt1 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b27"+"7d9ad9f146e"
+
 	makeOutputs([]byte(msg1), []byte(salt1))
-	fmt.Println()
+
+	const msg2 = "7d9ad9f146eMy Secret Data"
+	const salt2 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b27"
+
+
+	fmt.Println("\n\n\nExperiment #2************")
 	makeOutputs([]byte(msg2), []byte(salt2))
-	
+
 }
 
-func makeOutputs( msg []byte, salt []byte ){
-	fmt.Printf("Message: %v\n", string(msg))
+func makeOutputs(msg []byte, salt []byte) {
+	fmt.Printf("\nMessage: %v\n", string(msg))
 	fmt.Printf("Salt/Key: %v\n\n", string(salt))
 
-	
 	h1 := sha256.New()
-	h1.Write(append(salt,msg...))
-	
+	h1.Write(append(salt, msg...))
+
 	out1 := h1.Sum(nil)
 	fmt.Printf("Hash with Salt:\t%x\n", out1)
 
