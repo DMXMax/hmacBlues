25af918c6ac8fa098be5817b9fb52ca5982c0826
Glen Clarkson <29312988+DMXMax@users.noreply.github.com>
2020-03-01 12:10:08 -0800

    updated to show what happens with overlap

--- main.go
+++ main.go
@@ -7,30 +7,31 @@ import (
 )
 
 func main() {
-	const text = "Hello World"
-	const salt = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
-
-	fmt.Println("Hello, playground")
-	h1 := sha256.New()
-	h1.Write([]byte(salt + text))
-
-	fmt.Printf("%x\n", h1.Sum(nil))
-	fmt.Printf("%x\n", sha256.Sum256([]byte(salt+text)))
-	//fmt.Printf("%x\n", h1.Sum([]byte(text)))
-	//fmt.Printf("%x\n", sha256.Sum256([]byte(salt + text)))
-
-	//h2 := sha256.New()
-	//h2.Write([]byte(salt))
-	//h2.
-
-	mac := hmac.New(sha256.New, []byte(salt))
-	mac.Write([]byte(text))
-	fmt.Printf("%x\n", mac.Sum(nil))
-	fmt.Printf("%x\n", HMac256([]byte(text), []byte(salt)))
+	const msg1 = "Hello World"
+	const salt1 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
+	
+	const msg2 = "7d9ad9f146eHello World"
+	const salt2 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b27"
+	
+	makeOutputs([]byte(msg1), []byte(salt1))
+	fmt.Println()
+	makeOutputs([]byte(msg2), []byte(salt2))
+	
 }
 
-func HMac256(msg []byte, key []byte) []byte {
-	mac:= hmac.New(sha256.New, key)
+func makeOutputs( msg []byte, salt []byte ){
+	fmt.Printf("Message: %v\n", string(msg))
+	fmt.Printf("Salt/Key: %v\n\n", string(salt))
+
+	
+	h1 := sha256.New()
+	h1.Write(append(salt,msg...))
+	
+	out1 := h1.Sum(nil)
+	fmt.Printf("Hash with Salt:\t%x\n", out1)
+
+	mac := hmac.New(sha256.New, salt)
 	mac.Write([]byte(msg))
-	return mac.Sum(nil)
+	out2 := mac.Sum(nil)
+	fmt.Printf("HMAC with key:\t%x\n", out2)
 }
