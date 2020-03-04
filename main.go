655a8bdfee054ed3b4a8d94cd780b8e4f5d09119
Glen Clarkson <29312988+DMXMax@users.noreply.github.com>
2020-03-03 18:03:31 -0800

    Update for clarity

--- main.go
+++ main.go
@@ -9,22 +9,23 @@ import (
 func main() {
 	fmt.Println("Experiment #1*************")
 	const msg1 = "My Secret Data"
-	
+
 	//We're splitting the salt because we'll redistribute this later.
-	
-	const salt_pt1 = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b27"
-	const salt_pt2= "7d9ad9f146e"
-	
-	var salt = salt_pt1 + salt_pt2
+
+
+
+	var salt = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
 
 	makeOutputs([]byte(msg1), []byte(salt))
 
-	const msg2 = salt_pt2+ msg1
-	const salt2 = salt_pt1
-
-
 	fmt.Println("\n\n\nExperiment #2************")
-	makeOutputs([]byte(msg2), []byte(salt2))
+
+	const salt_pt1 = "a591a6d40bf420"
+	
+	const salt_pt2 = "404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e"
+	msg2 := msg1+salt_pt1
+
+	makeOutputs([]byte(msg2), []byte(salt_pt2))
 
 }
 
@@ -33,7 +34,7 @@ func makeOutputs(msg []byte, salt []byte) {
 	fmt.Printf("Salt/Key: %v\n\n", string(salt))
 
 	h1 := sha256.New()
-	h1.Write(append(salt, msg...))
+	h1.Write(append(msg, salt...))
 
 	out1 := h1.Sum(nil)
 	fmt.Printf("Hash with Salt:\t%x\n", out1)
