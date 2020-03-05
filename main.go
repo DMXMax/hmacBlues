1ec29de02c11c65fa018caa78622b6a3e7a531f9
Glen Clarkson <29312988+DMXMax@users.noreply.github.com>
2020-03-05 06:18:34 -0800

    More informative headers

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Experiment #1*************")
+	fmt.Println("Experiment #1: SHA-256 compared to HMAC-SHA256")
 	const msg1 = "My Secret Data"
 
 	//We're splitting the salt because we'll redistribute this later.
@@ -18,7 +18,7 @@ func main() {
 
 	makeOutputs([]byte(msg1), []byte(salt))
 
-	fmt.Println("\n\n\nExperiment #2************")
+	fmt.Println("\n\n\nExperiment #2: When Data overlaps with the Key.")
 
 	const salt_pt1 = "a591a6d40bf420"
 	
@@ -37,10 +37,10 @@ func makeOutputs(msg []byte, salt []byte) {
 	h1.Write(append(msg, salt...))
 
 	out1 := h1.Sum(nil)
-	fmt.Printf("Hash with Salt:\t%x\n", out1)
+	fmt.Printf("SHA-256 with Salt:\t%x\n", out1)
 
 	mac := hmac.New(sha256.New, salt)
 	mac.Write([]byte(msg))
 	out2 := mac.Sum(nil)
-	fmt.Printf("HMAC with key:\t%x\n", out2)
+	fmt.Printf("HMAC-SHA256 with key:\t%x\n", out2)
 }
